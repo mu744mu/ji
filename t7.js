c2197c4e7a650a4a975b7ac279027ce28e390fbe
mu744mu <80185284+mu744mu@users.noreply.github.com>
2021-05-29 19:36:09 +0300

    Create t7.js

---
+++ t7.js
@@ -0,0 +1,190 @@
+var textd =$($('a.label.label-primary.fl').html())[1].data;
+var htmltextd = $('a.label.label-primary.fl').html().replace(textd, "<span style='width: 70%;margin-top:8px' class='dots'>"+textd+"</span>")
+console.log($('a.label.label-primary.fl').html($(htmltextd)))
+                                                        
+                                                        
+                                                        
+                                                        
+                                                        
+
+var co = $('.label-primary').css("background-color").replace('\)',",0.8\)")
+var ssdf = $('a.label.label-primary.fl').attr("style")+"background-color:"+co+"!important"
+$('a.label.label-primary.fl').attr("style",ssdf).css("text-align","right")
+$('a.label.label-primary.fl button').removeClass("btn-success").addClass("btn-primary")
+$('a.label.label-primary.fl button').removeClass("btn-warning").addClass("btn-primary")
+$('a.label.label-primary.fl img ').removeClass("fl").addClass("fr").css("margin","2px")
+$('a.label.label-primary.fl button ').removeClass("btn-primary").removeClass("btn").removeClass("fr").addClass("fl").css("margin-top","4px")
+
+$(`
+
+
+        <style>
+@font-face {
+  font-family: 'Aref Ruqaa';
+  font-style: normal;
+  font-weight: 400;
+  src: url(styleone/WwkbxPW1E165rajQKDulIIIoVeo5.woff2) format('woff2');
+  unicode-range: U+0600-06FF, U+200C-200E, U+2010-2011, U+204F, U+2E41, U+FB50-FDFF, U+FE80-FEFC;
+}
+/* latin-ext */
+@font-face {
+  font-family: 'Aref Ruqaa';
+  font-style: normal;
+  font-weight: 400;
+  src: url(styleone/WwkbxPW1E165rajQKDulIIkoVeo5.woff2) format('woff2');
+  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
+/* latin */
+@font-face {
+  font-family: 'Aref Ruqaa';
+  font-style: normal;
+  font-weight: 400;
+  src: url(styleone/WwkbxPW1E165rajQKDulIIcoVQ.woff2) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}            div#tlogins .fr.borderg a,div#tlogins .fr.borderg {
+                border: none;
+                font-size: 13px!important;
+                color: `+$('.label-primary').css("background-color")+`;
+            }
+            .navbart {
+                margin: 2px;
+                width: 75%;
+                margin-top: -5%;
+
+            }
+            .navbart i {
+                color: `+$('.label-primary').css("background-color")+`;
+                font-weight: 400;
+                margin: 0 5px;
+                font-size: 15px !important;
+                margin-top: 4px;
+            }
+            .navbart span {
+                background-color: white!important;
+                text-decoration: underline;
+                margin-left: 1px;
+                color: `+$('.label-primary').css("background-color")+`;
+                font-weight: 400;
+                font-family: 'Aref Ruqaa', serif;
+                font-size: 15px !important;
+//                border: 1px solid;
+                width: 21%;
+                text-align: center;
+                border-radius: 15px;
+            }
+            div#l1 button,div#l2 button,div#l3 button {
+                background-color: white!important;
+                text-decoration: underline;
+                margin-left: 3px;
+                color: `+$('.label-primary').css("background-color")+`;
+                font-weight: 400;
+                font-size: 15px !important;
+                border: 1px solid;
+                width: 22%;
+                text-align: center;
+                border-radius: 15px;
+            }
+            a.label.label-primary.fl button {
+                background-color: white!important;
+                text-decoration: underline;
+                margin: 4px 1px 0 0!important;
+                color: `+$('.label-primary').css("background-color")+`;
+                font-weight: 400;
+                font-family: FontAwesome;
+                font-size: 15px !important;
+                border: 1px solid;
+                width: 25px;
+                text-align: center;
+                border-radius: 6px;
+                height: 24px;
+            }
+            ul.nav.nav-tabs li {
+                width: 33%;
+                border-bottom: 1px solid `+$('.label-primary').css("background-color")+`;
+            }
+            ul.nav.nav-tabs li.active {
+                width: 33%;
+                border-bottom: 1px solid transparent;
+            }
+            ul.nav.nav-tabs li a {
+                text-decoration: underline;
+                margin-left: 0px;
+                color: `+$('.label-primary').css("background-color")+`;
+                font-weight: 400;
+                width: 100%;
+                text-align: center;
+                padding: 0!important;
+            }
+            a.label.label-primary.fl {
+                padding: 0!important;
+            }
+            .nav-tabs>li.active>a, .nav-tabs>li.active>a:focus, .nav-tabs>li.active>a:hover {
+                border-left-color: `+$('.label-primary').css("background-color")+`;
+                border-top-color: `+$('.label-primary').css("background-color")+`;
+                border-right-color: `+$('.label-primary').css("background-color")+`;
+            }
+            a.label.label-primary.fl {
+                padding: 0!important;
+            }
+            div#l3>input {
+                width: 38%;
+                float: right;
+                margin-left: 1px;
+                border-radius: 3px;
+                text-align: center;
+                font-size: 12px!important;
+                border: `+$('.label-primary').css("background-color")+` solid 1px!important;
+                padding: 0;
+            }
+            div#l2>input {
+                width: 38%;
+                float: right;
+                margin-left: 1px;
+                border-radius: 3px;
+                text-align: center;
+                font-size: 12px!important;
+                border: `+$('.label-primary').css("background-color")+` solid 1px!important;
+                padding: 0;
+            }
+            div#l1>input {
+                width: 76%;
+                float: right;
+                margin-left: 1px;
+                border-radius: 3px;
+                text-align: center;
+                font-size: 12px!important;
+                border: `+$('.label-primary').css("background-color")+` solid 1px!important;
+                padding: 0;
+            }
+            div#l1 input:focus,div#l2 input:focus,div#l3 input:focus  {
+                outline: `+$('.label-primary').css("background-color")+` auto 1px;
+            }
+            label.fl.label.loginstat {
+                background-color: white;
+                color: `+$('.label-primary').css("background-color")+`;
+            }
+            span.s1.fa.fa-user.label.badgex.label-as-badge.label-success {
+                background-color: `+$('.label-primary').css("background-color")+`;
+                border: 1px solid;
+            }
+            font.des2 {
+                background-color: white!important;
+                text-decoration: underline;
+                margin: 0 15px;
+                font-weight: 400;
+                font-family: 'Aref Ruqaa', serif;
+                font-size: 15px !important;
+                border: 1px solid;
+                text-align: center;
+                border-radius: 15px;
+                padding: 0 10px;
+            }
+        </style>
+`).appendTo('head');
+
+$('div#l1 button,div#l2 button,div#l3 button').attr("class","")
+$('div#l1 br,div#l2 br,div#l3 br').remove()
+$("div#l1")[0].childNodes[4].data= "";
+$('div#l2 input#pass1').css("width","32%")
+$(".checkbox label").html($(".checkbox label input"))
+$(".checkbox").addClass("fl").css("margin-top", "1px").css("line-height", "0")
